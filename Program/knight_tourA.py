0bf1ddc6312865ec163dc3f831d7c2e0074fdcd7
hrm1810884 <82667808+hrm1810884@users.noreply.github.com>
2022-12-18 23:24:06 +0900

    Add files via upload

---
+++ Program/knight_tourA.py
@@ -0,0 +1,160 @@
+import copy
+import numpy as np
+import heapq
+
+mapSize = 6
+nc = 0
+inf = float('inf')
+
+class Board:
+    def __init__(self, board, x, y, depth, balance, goal):
+        self.board = board
+        self.knight_x = x
+        self.knight_y = y
+        self.depth = depth
+        self.balance = balance
+        self.goal = goal
+
+    def movable(self, x, y):
+        knight_x = self.knight_x + x
+        knight_y = self.knight_y + y
+        return knight_x >= 0 and knight_x < mapSize and knight_y >= 0 and knight_y < mapSize and self.board[knight_y][knight_x] == 0
+
+    def tryMove(self, x, y):
+        global OPEN, CONSIDERED, solved, nc
+        if (self.movable(x, y)):
+            board_next = copy.copy(self.board)
+            knight_x = self.knight_x+x
+            knight_y = self.knight_y+y
+            board_next[self.knight_y][self.knight_x] = 1
+            board_next[knight_y][knight_x] = 2
+            if (self.isnotCONSIDERED(board_next)):
+                if(abs(x) == 2):
+                    B_next = Board(board_next, knight_x, knight_y, self.depth+1, self.balance+1, self.goal)
+                else:
+                    B_next = Board(board_next, knight_x, knight_y, self.depth+1, self.balance-1, self.goal)
+                heapq.heappush(OPEN,[B_next.cost(),nc,B_next])
+                CONSIDERED.append(board_next)
+                nc += 1
+                board_test = copy.copy(board_next)
+                board_test[knight_y][knight_x] = 1
+                if ((board_test == self.goal).all()):
+                    solved = True
+
+    def isnotCONSIDERED(self, board):
+        board90 = np.rot90(board)
+        return self.notin(board) and self.notin(board90) and self.notin(np.rot90(board90)) and self.notin(np.rot90(board, 3)) and self.notin(np.flipud(board)) and self.notin(np.fliplr(board)) and self.notin(np.fliplr(board90)) and self.notin(np.flipud(board90))
+
+    def notin(self, board):
+        for b in CONSIDERED:
+            if ((board == b).all()):
+                return False
+        return True
+    
+    def cost(self):
+        access = 0
+        penalty = 0
+        if(self.movable(-1,-2)):
+            access += 1
+        if(self.movable(1,-2)):
+            access += 1
+        if(self.movable(2,-1)):
+            access += 1
+        if(self.movable(2,1)):
+            access += 1
+        if(self.movable(1,2)):
+            access += 1
+        if(self.movable(-1,2)):
+            access += 1
+        if(self.movable(-2,1)):
+            access += 1
+        if(self.movable(-2,-1)):
+            access += 1
+        if(self.board[2][1] and not self.board[0][0]):
+            penalty += 1
+        if(self.board[1][2] and not self.board[0][0]):
+            penalty += 1
+        if(self.board[1][mapSize-3] and not self.board[0][mapSize-1]):
+            penalty += 1
+        if(self.board[2][mapSize-2] and not self.board[0][mapSize-1]):
+            penalty += 1
+        if(self.board[mapSize-3][1] and not self.board[mapSize-1][0]):
+            penalty += 1
+        if(self.board[mapSize-2][2] and not self.board[mapSize-1][0]):
+            penalty += 1
+        if(self.board[mapSize-3][mapSize-2] and not self.board[mapSize-1][mapSize-1]):
+            penalty += 1
+        if(self.board[mapSize-2][mapSize-3] and not self.board[mapSize-1][mapSize-1]):
+            penalty += 1
+        if(not access or penalty >= 2):
+            return inf
+        return 4.0*mapSize*access + 1.5*mapSize*penalty + min(self.depth,mapSize*mapSize/5)
+
+
+def setInitBoard(center):
+    board_init = np.zeros((mapSize, mapSize), dtype="int8")
+    board_init[center][center] = 2
+    return board_init
+
+
+def setGoalBoard():
+    board_goal = np.ones((mapSize, mapSize), dtype="int8")
+    return board_goal
+
+
+OPEN = []
+CONSIDERED = []
+solved = False
+
+
+def execute(B_init):
+    global OPEN, CONSIDERED, nc
+    heapq.heappush(OPEN,[0,0,B_init])
+    CONSIDERED.append(B_init.board)
+    while (len(OPEN)):
+        element = heapq.heappop(OPEN)
+        B = element[2]
+        printBoard(B.board)
+        expand(B)
+        if (solved):
+            return nc
+    return -1
+
+
+def expand(b):
+    CONSIDERED.clear()
+    b.tryMove(-1, -2)
+    b.tryMove(1, -2)
+    b.tryMove(2, -1)
+    b.tryMove(2, 1)
+    b.tryMove(1, 2)
+    b.tryMove(-1, 2)
+    b.tryMove(-2, 1)
+    b.tryMove(-2, -1)
+
+
+def printBoard(b):
+    for y in range(mapSize):
+        for x in range(mapSize):
+            print(b[y][x], end='')
+            print(' ', end='')
+        print("\n")
+    print(str(nc) + "\n")
+            
+
+def main():
+    center = (mapSize - 1) // 2
+    board_init = setInitBoard(center)
+    board_goal = setGoalBoard()
+    B_init = Board(board_init, center, center, 0, 0, board_goal)
+    execute(B_init)
+    if (solved):
+        printBoard(board_goal)
+        print("solved!!")
+        print(str(nc) + " pattern were considered")
+    else:
+        print("failed...")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Program/knight_width.py
@@ -0,0 +1,115 @@
+import queue
+import copy
+import numpy as np
+
+mapSize = 5
+nc = 0
+
+
+class Board:
+    def __init__(self, board, x, y, depth, goal):
+        self.board = board
+        self.knight_x = x
+        self.knight_y = y
+        self.depth = depth
+        self.goal = goal
+
+    def movable(self, x, y):
+        knight_x = self.knight_x + x
+        knight_y = self.knight_y + y
+        return knight_x >= 0 and knight_x < mapSize and knight_y >= 0 and knight_y < mapSize and self.board[knight_y][knight_x] == 0
+
+    def tryMove(self, x, y):
+        global OPEN, CONSIDERED, solved, nc
+        if (self.movable(x, y)):
+            board_next = copy.copy(self.board)
+            knight_x = self.knight_x+x
+            knight_y = self.knight_y+y
+            board_next[self.knight_y][self.knight_x] = 1
+            board_next[knight_y][knight_x] = 2
+            if (self.isnotCONSIDERED(board_next)):
+                B_next = Board(board_next, knight_x, knight_y, self.depth+1, self.goal)
+                OPEN.put(B_next)
+                CONSIDERED.append(board_next)
+                nc += 1
+                board_test = copy.copy(board_next)
+                board_test[knight_y][knight_x] = 1
+                if ((board_test == self.goal).all()):
+                    solved = True
+
+    def isnotCONSIDERED(self, board):
+        board90 = np.rot90(board)
+        return self.notin(board) and self.notin(board90) and self.notin(np.rot90(board90)) and self.notin(np.rot90(board, 3)) and self.notin(np.flipud(board)) and self.notin(np.fliplr(board)) and self.notin(np.fliplr(board90)) and self.notin(np.flipud(board90))
+
+    def notin(self, board):
+        for b in CONSIDERED:
+            if ((board == b).all()):
+                return False
+        return True
+
+
+def setInitBoard(center):
+    board_init = np.zeros((mapSize, mapSize), dtype="int8")
+    board_init[center][center] = 2
+    return board_init
+
+
+def setGoalBoard():
+    board_goal = np.ones((mapSize, mapSize), dtype="int8")
+    return board_goal
+
+
+OPEN = queue.Queue()
+CONSIDERED = []
+solved = False
+
+
+def execute(B_init):
+    global OPEN, CONSIDERED, nc
+    OPEN.put(B_init)
+    CONSIDERED.append(B_init.board)
+    while (not OPEN.empty()):
+        b = OPEN.get()
+        printBoard(b.board)
+        expand(b)
+        if (solved):
+            return nc
+    return -1
+
+
+def expand(b):
+    CONSIDERED.clear()
+    b.tryMove(-1, -2)
+    b.tryMove(1, -2)
+    b.tryMove(2, -1)
+    b.tryMove(2, 1)
+    b.tryMove(1, 2)
+    b.tryMove(-1, 2)
+    b.tryMove(-2, 1)
+    b.tryMove(-2, -1)
+
+
+def printBoard(b):
+    for y in range(mapSize):
+        for x in range(mapSize):
+            print(b[y][x], end='')
+            print(' ', end='')
+        print("\n")
+    print(str(nc) + "\n")
+
+
+def main():
+    center = (mapSize - 1) // 2
+    board_init = setInitBoard(center)
+    board_goal = setGoalBoard()
+    B_init = Board(board_init, center, center, 0, board_goal)
+    execute(B_init)
+    if (solved):
+        print("solved!!")
+        print(str(nc) + " pattern were considered")
+    else:
+        print("failed...")
+
+
+if __name__ == "__main__":
+    main()
